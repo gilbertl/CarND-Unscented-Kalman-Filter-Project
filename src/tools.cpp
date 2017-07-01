a54e78de22b8ca5dfd1393856e222264acf51123
Gilbert King Fai Leung <gilbertleung@gilbertleung-macbookpro3.roam.corp.google.com>
2017-07-01 13:32:12 +0800

    Got radar to work

--- src/tools.cpp
+++ src/tools.cpp
@@ -10,9 +10,28 @@ Tools::Tools() {}
 Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+                              const vector<VectorXd> &ground_truths) {
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  //accumulate squared residuals
+  int n = 0;
+  for(int i=0; i < estimations.size(); ++i){
+      VectorXd estimation = estimations[i];
+      VectorXd ground_truth = ground_truths[i];
+      if (estimation.size() == 0) {
+          continue;
+      }
+      if (ground_truth.size() != estimation.size()) {
+          continue;
+      }
+      VectorXd diff = estimation - ground_truth;
+      VectorXd diff2 = diff.array() * diff.array();
+      rmse += diff2;
+      n += 1;
+  }
+  rmse /= n;
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
